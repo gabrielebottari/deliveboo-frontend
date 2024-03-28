f421dd70d986e20a4fd653d0eaceae15a6da31bf
Michail Spinelli <michailspinelli96@gmail.com>
2024-03-28 11:11:39 +0100

    bozza footer, inizio struttura homepage

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,11 +12,92 @@ export default {
 </script>
 
 <template>
-    <div class="container-fluid bg-black text-white">
-        <div>
-            sinistra
-        </div>
+    <div class="container-fluid bg-black text-white p-5">
         
+        <!--Card del footer contenenti link ed altro-->
+        <div class="row">
+            <div class="col-sm-3 mb-3 mb-sm-0">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Scopri Deliveboo</h5>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Note legali</h5>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Aiuto</h5>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Porta Deliveroo con te</h5>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                        <a href="">Investitori</a>
+                        <br>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--Loghi dei social finali-->
+
+        <div class="social-logo">
+            <p>
+                Facebook
+            </p>
+            <p>
+                Instagram
+            </p>
+            <p>
+                Twitter
+            </p>
+        </div>
+
+
     </div>
 </template>
 
