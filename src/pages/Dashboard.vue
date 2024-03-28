d67aaccd0f3982b9ebc900bb6a02c9c546375ec4
PaoloTarcisio <paoloc50@gmail.com>
2024-03-28 16:11:15 +0100

    progressi dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,50 +1,82 @@
 <script>
 export default {
-    data() {
-        return {
-            cards:[
-                {
-                    title: 'Il Mio Menù',
-                    img:'https://media.istockphoto.com/id/96430985/it/foto/pancake.jpg?s=2048x2048&w=is&k=20&c=27bCjgpBLWif3mvJzRuHQOugikYP6xb34mzVvRIp00o='
-                },
-                {
-                    title: 'Il Mio Menù',
-                    img:'https://media.istockphoto.com/id/96430985/it/foto/pancake.jpg?s=2048x2048&w=is&k=20&c=27bCjgpBLWif3mvJzRuHQOugikYP6xb34mzVvRIp00o='
-                },
-                {
-                    title: 'Il Mio Menù',
-                    img:'https://media.istockphoto.com/id/96430985/it/foto/pancake.jpg?s=2048x2048&w=is&k=20&c=27bCjgpBLWif3mvJzRuHQOugikYP6xb34mzVvRIp00o='
-                },
-            ]
-        }
-    },
-}
+  data() {
+    return {
+      cards: [
+        {
+          title: "Il Mio Menù",
+          img:
+            "https://media.istockphoto.com/id/96430985/it/foto/pancake.jpg?s=2048x2048&w=is&k=20&c=27bCjgpBLWif3mvJzRuHQOugikYP6xb34mzVvRIp00o=",
+          link: "",
+        },
+        {
+          title: "Il Mio Menù",
+          img:
+            "https://media.istockphoto.com/id/96430985/it/foto/pancake.jpg?s=2048x2048&w=is&k=20&c=27bCjgpBLWif3mvJzRuHQOugikYP6xb34mzVvRIp00o=",
+          link: "",
+        },
+        {
+          title: "Il Mio Menù",
+          img:
+            "https://media.istockphoto.com/id/96430985/it/foto/pancake.jpg?s=2048x2048&w=is&k=20&c=27bCjgpBLWif3mvJzRuHQOugikYP6xb34mzVvRIp00o=",
+          link: "",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-
 <template>
+    <main>
 
+                    <!-- cards created using a for cicle -->
 
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <div class="cards" v-for="card in cards">
-                    <div class="card m-5">
-                        <div class="row g-0">
-                            <div class="col-md-4">
-                                <img :src=card.img class="img-fluid rounded-start" alt="...">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ card.title }}</h5>
-                                </div>
-                            </div>
+        <div class="container">
+            <div class="row">
+                <div class="col cards" v-for="card in cards">
+                    <div class="my-card">
+                        <div class="card-img">
+                            <div>i'm the img of this card</div>
                         </div>
+                        <div class="card-content">and i'm the text of this card</div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-
+    </main>
 </template>
+
+<style lang="scss">
+
+.row * {
+    padding: 0;
+}
+
+main {
+  background: linear-gradient(45deg, #fffb00, #12c600);
+  opacity: 0.8;
+
+    .container {
+        max-width: 60%;
+
+        .cards {
+
+            width: calc(90% / 3);
+            
+            
+            border: 5px solid black;
+            
+            .my-card {
+                background: lightgrey;
+                height: 200px;
+    
+                .card-img {
+                    background: darkgrey;
+                }
+            }
+        }     
+    }
+}
+
+</style>
