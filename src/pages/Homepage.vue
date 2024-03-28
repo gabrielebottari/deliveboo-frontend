dcc853beb3b5e188deaa4df8c7fe74fa98727c2c
Michail Spinelli <michailspinelli96@gmail.com>
2024-03-28 17:18:05 +0100

    scheletro home page

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -16,36 +16,59 @@ export default {
     <!--Header che si ricollega all'header generale-->
     <header>
         <!--Container principale dell'header della homepage, che si ricollegherà all'header generale-->
-        <div class="container-fluid homepage-header bg-primary">
+        <div class="row container-fluid">
 
-            <h2>
-                I migliori piatti che puoi trovare in circolazione
-            </h2>
-                    
-            <!--ricerca per indirizzo-->
-            
-            <nav class="navbar bg-danger">
-            <div class="container-fluid text-center">
-                <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+        <!--QUI è LA PRIMA PARTE DELLA HOMEPAGE-->
+            <!--Questa è la parte sinistra della homepage-->
+            <div class="col container-fluid homepage-header-left">
+                <!--vuoto-->
             </div>
-            </nav>
+            <!--Questa è la parte destra della homepage-->
+            <div class="col container-fluid homepage-header-right pt-5">
 
+                <h2 class="text-center title-research-bar">
+                    I migliori piatti che puoi trovare in circolazione
+                </h2>
+                        
+                <!--ricerca per indirizzo-->
+                <nav class="navbar justify-content-around" id="search-address">
+                        <form class="d-flex" role="search">
+                        <!--placeholder con indirizzo-->
+                        <input class="form-control me-2" type="search" placeholder="Inserisci il tuo indirizzo" aria-label="Search">
+                        <!--tasto per far partire la ricerca-->
+                        <button class="btn btn-outline-success bg-white" type="submit">Cerca</button>
+                        </form>
+                </nav>       
+
+            </div>
         </div>
         
     </header>
+
+    <!---->
+    <div>
+
+    </div>
    
 </template>
 
 <style lang="scss" scoped>
-    .homepage-header {
-        height: 400px;
-        padding: 50px 500px 50px 500px;
+    header{
+        background-image: url(/src/assets/img/deliveboo.png);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
     }
-    .homepage-header > h2 {
-        margin-bottom: 100px;
-        margin-left: 90px;
+    .homepage-header-left {
+        height: 700px;
+    }
+    .homepage-header-left > img {
+        max-width: 300px;
+    }
+    #search-address{
+      margin-top: 100px;
+    }
+    .title-research-bar{
+        font-size: 40px;
     }
 </style>
