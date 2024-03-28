a32d7ace871f3bd726970c823b1e0daae465e662
Michail Spinelli <michailspinelli96@gmail.com>
2024-03-28 18:19:19 +0100

    homepage parte maps, e inizio bottoni

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,4 +1,5 @@
-<script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js">
+
 export default {
     data() {
         return {
@@ -45,11 +46,82 @@ export default {
         
     </header>
 
-    <!---->
-    <div>
+    <!--Parte dell'homepage che conterrà le mappe oppure un consiglio di ristoranti-->
+    <div class="p-5 bg-danger justify-content-center d-flex">
+        <!--homepage, mappe?? consigli ristoranti??-->
+        <div class="card mb-3 p-5" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                <img src="..." class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Esempio</h5>
+                    <p class="card-text">Mappe? Ristoranti consigliati?</p>
+                    <p class="card-text"><small class="text-body-secondary">Da decidere</small></p>
+                </div>
+                </div>
+            </div>
+        </div>
 
     </div>
    
+   <!--parte dell'homepage che contiene una serie di bottoni per link-->
+   <div class="p-5">
+
+        <!--bottoni per le città-->
+        <div>
+
+            <div class="text-center mb-3">
+                Immagine che ricorda città
+            </div>
+            <h2 class="text-center mb-5">
+                Città Principali
+            </h2>
+
+            <!--div contenente bottoni per le città-->
+            <div class="d-flex justify-content-around">
+                
+                <button type="button" class="btn btn-danger dropdown-toggle py-2 px-5" data-bs-toggle="dropdown" aria-expanded="true">
+                    Roma
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Parioli</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                </ul>
+
+                <button type="button" class="btn btn-danger dropdown-toggle py-2 px-5" data-bs-toggle="dropdown" aria-expanded="false">
+                    Action
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                </ul>
+
+                <button type="button" class="btn btn-danger dropdown-toggle py-2 px-5" data-bs-toggle="dropdown" aria-expanded="false">
+                    Action
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Separated link</a></li>
+                </ul>
+
+                
+
+            </div>
+            
+        </div>
+
+   </div>
 </template>
 
 <style lang="scss" scoped>
