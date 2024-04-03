53fb905e34349d5b938b29910c7ca6a09920291c
PaoloTarcisio <paoloc50@gmail.com>
2024-04-03 14:44:14 +0200

    pt.2

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from './pages/Homepage.vue';
-import Dashboard from './pages/Dashboard.vue';
 import Carrello from './pages/Carrello.vue';
 import Menu from './Menu.vue';
 import NotFound from './pages/NotFound.vue';
