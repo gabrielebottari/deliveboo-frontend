abcb40d3846e3a6612f48884a5fd48602d08e7b2
PaoloTarcisio <paoloc50@gmail.com>
2024-04-09 18:51:35 +0200

    commit prova

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { reactive } from 'vue';
-
-export const store = reactive ({
-    restaurantsUrl: 'http://127.0.0.1:8000/api/restaurants',
-    restaurants:[],
-
-    typesUrl: 'http://127.0.0.1:8000/api/types',
-    types:[],
-});
